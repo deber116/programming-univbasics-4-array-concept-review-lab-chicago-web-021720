ec99b1d750bc209027f5f0e9992abe1cd9326e6c
David Eber <deber116@gmail.com>
2020-01-29 17:19:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,12 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  
+  answer = 0 
+  array.length.times do |index|
+    if array[index] > answer
+      answer = array[index]
+    end
+  end
 end
 
 def find_min_value(array)
